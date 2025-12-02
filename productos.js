f4d7ed83e2fbb23891b010e857d760b6d7421898
coqueta <fmalbarian@gmail.com>
2025-12-02 11:18:04 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
             descripcion: 'Art: 300 TV'
         },
          {
-            imagen: 'catalogo/ColalesstTiraRegFP.jpg',
+            imagen: 'catalogo/ColalesstTiraRegFP.JPG',
             nombre: 'Colaless Tira Regulable Frente Puntilla',
             descripcion: 'Art: 301 TV'
         },
@@ -147,6 +147,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
