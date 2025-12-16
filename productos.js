2d75b294749b7088bb3f5a332db503b20eb8606e
coqueta <fmalbarian@gmail.com>
2025-12-15 21:40:15 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
             descripcion: 'Art: 2001 TS-M-L-XL'
         },
         {
-            imagen: 'catalogo/bombachaNena.jpg',
+            imagen: 'catalogo/bombachaNena.PNG',
             nombre: 'Bombacha Nena Gatitos/Capybara',
             descripcion: 'Art: 201.3 T1-2-3'
         },
@@ -131,6 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
